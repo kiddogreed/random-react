fc63882ea0428215379c059ed05dde4ca54c76d5
john russelle <kiddogreed@gmail.com>
2022-12-03 23:40:42 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App= ()=> {
+  const test = 'test'
+  const isNameShowing = false
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>hasdas:{isNameShowing? test:'someone'}</h1>
+     {
+      isNameShowing ? (
+         <>
+           true
+         </>
+       ):(
+         <>
+           false
+         </>
+       )
+     }
     </div>
   );
 }
