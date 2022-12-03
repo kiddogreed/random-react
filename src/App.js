03a13865a4bc670127c76d325d3ef7f0cf9fa931
john russelle <kiddogreed@gmail.com>
2022-12-04 00:11:05 +0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
+
+const Person = (props) =>{
+  return (
+    <>
+      <h1>First Name:{props.firstName}</h1>
+      <h2>Last Name :{props.lastName}</h2>
+      <h3>Age: {props.age}</h3>
+    </>
+  )
+}
 
 const App= ()=> {
   const test = 'test'
   const isNameShowing = false
 
+  const [counter, setCounter] = useState(0)
+
+
+ const increaseMe = () =>{
+   //alert('increase') to test the function
+  //use setState setCounter
+  setCounter((prevCount) =>prevCount +1) 
+  }
+ const decreaseMe = () =>{
+   //alert('decrease')
+   setCounter((prevCount)=>prevCount -1)
+   
+  }
+
+ useEffect(() =>{
+   //setCounter(100)
+   alert('you change the counter'+counter)
+ },[counter])
+  
+  
+  
+  
+
   return (
     <div className="App">
-     <h1>hasdas:{isNameShowing? test:'someone'}</h1>
+     <button onClick={increaseMe}>+</button><br></br>
+     {counter}
+     <br></br>
+     <button onClick={decreaseMe}>-</button><br></br>
+     
+  
      {
       isNameShowing ? (
          <>
